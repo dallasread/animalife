134e22824670c240c1fc06bf5568e62279dc83de
Dallas Read <dallas@excitecreative.ca>
2021-07-22 16:16:21 -0300

    Turtley!

---
+++ src/components/app/characters.js
@@ -0,0 +1,14 @@
+import Doggie from '@/assets/characters/doggie.svg'
+import Turtley from '@/assets/characters/turtley.svg'
+
+export default [{
+    name: 'Doggie',
+    image: Doggie,
+    width: 20,
+    yOffset: -1
+  }, {
+    name: 'Turtley',
+    image: Turtley,
+    width: 6,
+    yOffset: 0
+}]
