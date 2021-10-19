8475c63e947608c29bc11229bca6239fcca78346
Dallas Read <dallas@excitecreative.ca>
2021-10-19 11:40:25 -0300

    gh-pages

---
+++ precache-manifest.9307d78628d60e6b02232b88a9a746f3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b9b02739b907d660c5",
+    "url": "/animalife/css/app.3cd41d0c.css"
+  },
+  {
+    "revision": "3118b7f4e7160b4cd86ad75ad9e2a074",
+    "url": "/animalife/fonts/Pixeboy.3118b7f4.ttf"
+  },
+  {
+    "revision": "9792b838310fe1bd8e2dee162c9307cd",
+    "url": "/animalife/img/bone.9792b838.svg"
+  },
+  {
+    "revision": "5dbfbb688ea261bf6ee99ad8ae95284e",
+    "url": "/animalife/img/chihuahua.5dbfbb68.svg"
+  },
+  {
+    "revision": "fe781026245bde4beb2746110f07ba81",
+    "url": "/animalife/img/doggie.fe781026.svg"
+  },
+  {
+    "revision": "33f5a0dbd5630f27f2ac1c5591db344c",
+    "url": "/animalife/img/fish.33f5a0db.svg"
+  },
+  {
+    "revision": "48c1df48dc3e50227a134ea080348644",
+    "url": "/animalife/img/grass.48c1df48.svg"
+  },
+  {
+    "revision": "9b0c31115c1c706bb19559895d9ee782",
+    "url": "/animalife/img/heart.9b0c3111.svg"
+  },
+  {
+    "revision": "ce7a9851002df21b582a20ecfc3d8869",
+    "url": "/animalife/img/kitty.ce7a9851.svg"
+  },
+  {
+    "revision": "9e1c08d14365a0ddffb00840d061234e",
+    "url": "/animalife/img/logo.9e1c08d1.svg"
+  },
+  {
+    "revision": "5254e3cb15f0136ec6b957d634e2e660",
+    "url": "/animalife/img/rainbow-bone.5254e3cb.svg"
+  },
+  {
+    "revision": "8cf15af534ea2812b0f6bba51cd1fb96",
+    "url": "/animalife/img/rainbow-fish.8cf15af5.svg"
+  },
+  {
+    "revision": "b072e7fb832c25d272b5701aa45bdbfb",
+    "url": "/animalife/img/rainbow-seaweed.b072e7fb.svg"
+  },
+  {
+    "revision": "074365d3b80b7b70e699fe8d1cd6d1af",
+    "url": "/animalife/img/seaweed.074365d3.svg"
+  },
+  {
+    "revision": "d114383e749a96e3310045ed81d4d5fc",
+    "url": "/animalife/img/sky.d114383e.svg"
+  },
+  {
+    "revision": "89d8344c07e0d61e52f30a1d0ef7aed2",
+    "url": "/animalife/img/snail.89d8344c.svg"
+  },
+  {
+    "revision": "2015ce247575571e528ee8b73f38a0f3",
+    "url": "/animalife/img/turtley.2015ce24.svg"
+  },
+  {
+    "revision": "2bcb40af336d162938cb55d1b88d9040",
+    "url": "/animalife/index.html"
+  },
+  {
+    "revision": "67b9b02739b907d660c5",
+    "url": "/animalife/js/app.4610f365.js"
+  },
+  {
+    "revision": "1ddc690f886c81d2effd",
+    "url": "/animalife/js/chunk-vendors.59d0501e.js"
+  },
+  {
+    "revision": "6e903d35e54d30dbf9c02cf84d0e6d48",
+    "url": "/animalife/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/animalife/robots.txt"
+  }
+]);
